d22313028f873dde33728db86961efe7db4514b2
HyunJoon_Cho <104756108+HyunJoonCho92@users.noreply.github.com>
2024-06-20 17:24:35 +0900

    [Silver III] Title: 구간 합 구하기 4, Time: 2488 ms, Memory: 258984 KB -BaekjoonHub

--- 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.java
+++ 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.java
@@ -1,35 +1,32 @@
 import java.util.Scanner;
 
 public class Main {
+    public static void main(String[] args) {
 
-	public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
 
-		Scanner sc = new Scanner(System.in);
-		
-		int N = sc.nextInt();
-		int M = sc.nextInt();
-		
-		int arr[] = new int[N];
-		int sum[] = new int[N];
-		
-		for(int i = 0 ; i < N;i++) {
-			arr[i] = sc.nextInt();
-			if(i == 0) {
-				sum[i] = arr[i];
-				continue;
-			}
-			sum[i] = sum[i-1] + arr[i];
-		}
-		
-		for(int i = 0 ; i < M;i++) {
-			int a = sc.nextInt();
-			int b = sc.nextInt();
-			
-			if(a == 1) {
-				System.out.println(sum[b-1]);
-				continue;
-			}
-			System.out.println(sum[b-1] - sum[a-2]);
-		}
-	}
+        int N = sc.nextInt();
+
+        int M = sc.nextInt();
+
+        int arr[] = new int[N];
+        for(int i = 0 ; i < N;i++) {
+            arr[i] = sc.nextInt();
+            if(i == 0)
+                continue;
+            else {
+                arr[i] = arr[i] + arr[i-1];
+            }
+        }
+
+        for(int i = 0 ; i < M;i++) {
+            int start = sc.nextInt();
+            int end = sc.nextInt();
+            if(start - 2 >= 0)
+                System.out.println(arr[end-1] - arr[start-2]);
+            else
+                System.out.println(arr[end-1]);
+        }
+
+    }
 }
