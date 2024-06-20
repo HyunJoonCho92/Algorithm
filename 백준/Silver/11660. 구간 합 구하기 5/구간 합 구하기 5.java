6932b633650a754fc8744504a645a08466e3050d
HyunJoon_Cho <104756108+HyunJoonCho92@users.noreply.github.com>
2024-06-20 18:31:07 +0900

    [Silver I] Title: 구간 합 구하기 5, Time: 2612 ms, Memory: 320480 KB -BaekjoonHub

--- 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.java
+++ 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.java
@@ -1,60 +1,40 @@
 import java.util.Scanner;
-import java.util.Arrays;
 
 public class Main {
+    public static void main(String[] args) {
 
-	public static void main(String[] args) {
-		
-		Scanner sc = new Scanner(System.in);
-		int N = sc.nextInt();
-		int M = sc.nextInt();
-		
-		int arr[][] = new int[N+1][N+1];
-		int sum[][] = new int[N+1][N+1];
-		
-		for(int i = 1 ; i <= N;i++) {
-			for(int j = 1 ; j <= N ;j++) {
-				int num = sc.nextInt();
-				sum[i][j] = sum[i][j-1] + sum[i-1][j] - sum[i-1][j-1] + num;
-			}
-		}
-		
-		//구간 합 sum 배열에 구하기
-//		for(int i = 1; i <= N;i++) {
-//			for(int j = 1 ; j <= N ;j++) {
-//				if(i == 1) {
-//					if(j == 1) {
-//						sum[i][j] = arr[i][j];
-//					}
-//					else {
-//						sum[i][j] = sum[i][j-1] + arr[i][j];
-//					}
-//				}
-//				else {
-//					if(j == 1 ) {
-//						sum[i][j] = sum[i-1][j] + arr[i][j];
-//					}
-//					else {
-//						sum[i][j] = sum[i][j-1] + sum[i-1][j] - sum[i-1][j-1] + arr[i][j];
-//					}
-//				}
-//			}
-//		}
-		
-//		for(int i = 1; i <= N;i++) {
-//			for(int j = 1 ; j <= N ;j++) {
-//				sum[i][j] = sum[i][j-1] + sum[i-1][j] - sum[i-1][j-1] + arr[i][j];
-//			}
-//		}
-		
-		for(int i = 0; i < M;i++) {
-			int x1 = sc.nextInt();
-			int y1 = sc.nextInt();
-			int x2 = sc.nextInt();
-			int y2 = sc.nextInt();
+        Scanner sc = new Scanner(System.in);
 
-			System.out.println(sum[x2][y2] - sum[x2][y1-1] - sum[x1-1][y2] + sum[x1-1][y1-1]);
-		
-		}
-	}
+        int N = sc.nextInt();
+
+        int M = sc.nextInt();
+
+        int arr[][] = new int[N+1][N+1];
+        for (int i = 1; i <= N; i++) {
+            for (int j = 1; j <= N; j++) {
+                arr[i][j] = sc.nextInt();
+
+                arr[i][j] = arr[i][j] + arr[i - 1][j] + arr[i][j - 1] - arr[i - 1][j - 1];
+            }
+        }
+
+//        for (int i = 1; i <= N; i++) {
+//            for (int j = 1; j <= N; j++) {
+//                System.out.print(arr[i][j] + " ");
+//            }
+//            System.out.println();
+//        }
+
+
+        for (int i = 0; i < M; i++) {
+            int startX = sc.nextInt();
+            int startY = sc.nextInt();
+            int endX = sc.nextInt();
+            int endY = sc.nextInt();
+
+            int answer = arr[endX][endY] - arr[endX][startY-1] - arr[startX-1][endY] + arr[startX-1][startY-1];
+            System.out.println(answer);
+        }
+
+    }
 }
